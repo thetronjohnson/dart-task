accc508b6d85d5448a448f4aea1cd3749f5faf5a
Kiran Johns <kiranjohns369@gmail.com>
2020-03-17 13:43:10 +0530

    class reddesign

--- task.dart
+++ task.dart
@@ -1,45 +1,71 @@
-class Person{
+class Person {
   String name;
   String stack;
   String role;
-  Person(this.name);
-
-  void addStacks(stack){
-      this.stack = stack;
-  }
-
-}
-
-class Mentor extends Person{
   int time;
-  Mentor(String name) : super(name);
-  void setMentororLearner(){
-    this.role = "Mentor";
-  }
-  void setAvailableTime(time){
-    this.time = time;
-  }
-}
 
-class Learner extends Person{
-  Learner(String name) : super(name);
-  void setMentororLearner(){
-    this.role = "Learner";
+  Person(this.name,this.role,this.stack);
+
+  String addStacks(stack){
+    this.stack = stack;
+    return this.stack;
   }
 
-  void getMentor(stack,time){
-    
+  String setMentororLearner(role){
+    if(role=="Learner"){
+      this.role = role;
+      return this.role;
+    }
+    else if(role=="Mentor"){
+      this.role = role;
+      return this.role;
+    }
+    this.role = "Undefined";
+    return "Undefined";
   }
+  int setAvailableTime(time){
+    if(this.role=="Learner"){
+      this.time = 0;
+      return 0;
+    }
+    else if(this.role=="Mentor"){
+      this.time = time;
+      return this.time;
+    }
+    return 0;
+  }
+
+  void getMentors(list,stack,time){
+    if(this.role=="Learner"){
+      for(var i in list){
+        if(i.stack==stack && i.time==time){
+          print("${i.name}");
+        }
+
+      }
+      
+    }
+  }
+
 }
 
 void main(){
+  var a = Person("Ash","Learner","Web");
+  print("${a.name}");
+  print("${a.role}");
+  print("${a.stack}");
+  print("---------\n");
+  var b = Person("Red","Mentor","Web");
+  b.setAvailableTime(50);
+  var c = Person("Blue","Mentor","Web");
+  c.setAvailableTime(60);
+  var d = Person("Green","Mentor","Futter");
+  d.setAvailableTime(100);
+  var e = Person("Yellow","Mentor","Web");
+  e.setAvailableTime(50);
+  List <Person> list = [b,c,d,e];
 
-  var m = Mentor("Red");
-  var l = Learner("Ash");
-  m.setAvailableTime(50);
-  l.addStacks("Flutter");
-  m.addStacks("Web");
-  print("${m.time}");
-  print("${l.stack}");
+  a.getMentors(list, "Web", 50);
+
 
 }
